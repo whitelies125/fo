7b942e0ca04feba20179e28f06afa131ddbd14af
whitelies125 <lzb125125@gmail.com>
2025-05-16 02:55:33 +0800

    drop first 2 input parameter.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@ int main(int argc, char* argv[])
         return 1;
     }
     const auto argv_range = std::ranges::subrange(argv, argv + argc);
-    const auto paras = argv_range | std::views::transform([](const char* arg) { return std::string(arg); })
+    const auto paras = argv_range | std::views::drop(2)
+                                  | std::views::transform([](const char* arg) { return std::string(arg); })
                                   | std::ranges::to<Paras>();
     auto condition = [&argv](const auto& it) { return it.match_name.starts_with(argv[1]); };
     if (const auto res = std::ranges::find_if(g_item, condition); res != std::end(g_item)) {
